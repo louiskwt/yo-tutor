8ffc98dc7bc7ef4b5865fe9cee76dd3dceb6fc3e
Louis Tsang <kawingt2@gmail.com>
2023-12-31 16:04:22 +0800

    launched bot

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+require("dotenv").config();
+
 const express = require("express");
 const nunjucks = require("nunjucks");
+const tgBot = require("./bot");
 
 const app = express();
 const port = 3000;
@@ -12,6 +15,14 @@ nunjucks.configure("views", {
 app.get("/", (req, res) => {
   res.render("index.html");
 });
+
+tgBot.start((ctx) => ctx.reply("Welcome!"));
+
+tgBot.help((ctx) => ctx.reply("Send me a sticker"));
+
+tgBot.on("sticker", (ctx) => ctx.reply("👍"));
+
+tgBot.launch();
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
