5347e974e895acd70b0ec4d08cf848e3a35e6551
Louis Tsang <kawingt2@gmail.com>
2024-01-01 23:09:01 +0800

    updated profile

--- bot/menu.js
+++ bot/menu.js
@@ -1,8 +1,36 @@
 const menu = {
-  rt: "rt",
-  rs: "rs",
-  tp: "tp",
-  stp: "stp",
+  rt: {
+    command: "rt",
+    description: "登記成為導師",
+  },
+  rs: {
+    command: "rs",
+    description: "登記成為學生",
+  },
+  tp: {
+    command: "mytprofile",
+    description: "查看你的導師資料",
+  },
+  stp: {
+    command: "mystprofile",
+    description: "查看你的學生資料",
+  },
+  utp: {
+    command: "updatetprofile",
+    description: "更新你的導師資料",
+  },
+  ustp: {
+    command: "updatestprofile",
+    description: "更新你的學生資料",
+  },
+  tc: {
+    command: "tc",
+    description: "尋找導師",
+  },
+  sc: {
+    command: "sc",
+    description: "尋找學生",
+  },
 };
 
 module.exports = menu;
