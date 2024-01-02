08350d345a21aafd40c3fabee32f278bf8d08921
Louis Tsang <kawingt2@gmail.com>
2024-01-02 08:22:00 +0800

    refactored menu command

--- bot/index.js
+++ bot/index.js
@@ -4,17 +4,16 @@ const menu = require("./menu");
 
 const TG_BOT_TOKEN = process.env.TG_BOT_TOKEN;
 
+const commandArr = Object.keys(menu).map((key) => {
+  return {
+    command: menu[key].command,
+    description: menu[key].description,
+  };
+});
+
 const tgBot = new Telegraf(TG_BOT_TOKEN);
-tgBot.telegram.setMyCommands([
-  {command: menu.rt.command, description: menu.rt.description},
-  {command: menu.rs.command, description: menu.rs.description},
-  {command: menu.tp.command, description: menu.tp.description},
-  {command: menu.stp.command, description: menu.stp.description},
-  {command: menu.utp.command, description: menu.utp.description},
-  {command: menu.ustp.command, description: menu.ustp.description},
-  {command: menu.tc.command, description: menu.tc.description},
-  {command: menu.sc.command, description: menu.sc.description},
-]);
+
+tgBot.telegram.setMyCommands(commandArr);
 
 tgBot.start(async (ctx) => {
   ctx.reply("哈囉～歡迎使用 Yo Tutor\nYoTutor 是按月費🈷️ / 充值形式運行的補習配對服務，當你每次想要聯絡你心怡的導師或學生時，系統會因應你的能量點或月費計劃去決定能可讓你聯絡對方\n  \n 登記成為導師，請按 /rt \n 登記成為學生，請按 /rs \n 查看導師資料，請按 /tp \n 查看學生資料，請按 /stp \n ");
