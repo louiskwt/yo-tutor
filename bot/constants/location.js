7e2ba4e9fa74424251d528a6b5d3ab8f492b2d9f
Louis Tsang <kawingt2@gmail.com>
2024-01-02 22:38:11 +0800

    added constants

---
+++ bot/constants/location.js
@@ -0,0 +1,30 @@
+const DISTRICTS = {
+  CW: "中西區",
+  E: "東區",
+  S: "南區",
+  SSP: "深水埗區",
+  KC: "九龍城區",
+  WTS: "黃大仙區",
+  YTM: "油尖旺區",
+  KT: "觀塘區",
+  TW: "荃灣區",
+  TM: "屯門區",
+  YL: "元朗區",
+  N: "北區",
+  TP: "大埔區",
+  ST: "沙田區",
+  SK: "西貢區",
+  IL: "離島區",
+  WC: "灣仔區",
+};
+
+const AREA = {
+  HK: "香港島",
+  KLN: "九龍",
+  NT: "新界",
+};
+
+module.exports = {
+  DISTRICTS,
+  AREA,
+};
