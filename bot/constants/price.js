d2eda4b9594176601c1aa571e955007e02043846
Louis Tsang <kawingt2@gmail.com>
2024-01-02 23:01:08 +0800

    added price range

---
+++ bot/constants/price.js
@@ -0,0 +1,12 @@
+const PRICE = {
+  first: "100 - 200 HKD / hr",
+  second: "200 - 300 HKD / hr",
+  third: "300 - 400 HKD / hr",
+  fourth: "400 - 500 HKD / hr",
+  fifth: "500 - 600 HKD / hr",
+  other: "Other",
+};
+
+module.exports = {
+  PRICE,
+};
