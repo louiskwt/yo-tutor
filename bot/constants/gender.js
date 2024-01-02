0e86b63715ca1c17b519d44ee73caa7b879cf16c
Louis Tsang <kawingt2@gmail.com>
2024-01-02 22:58:59 +0800

    added gender and subjects const

---
+++ bot/constants/gender.js
@@ -0,0 +1,8 @@
+const GENDER = {
+  M: "男",
+  F: "女",
+};
+
+module.exports = {
+  GENDER,
+};
---
+++ bot/constants/subjects.js
@@ -0,0 +1,31 @@
+const SUBJECTS = {
+  ALL: "全科",
+  PCHIN: "小學中文",
+  PENG: "小學英文",
+  PMATH: "小學數學",
+  PSCI: "小學常識",
+  MATH: "數學",
+  PHY: "物理",
+  CHEM: "化學",
+  BIO: "生物",
+  HIST: "歷史",
+  CHIST: "中國歷史",
+  ECON: "經濟",
+  GEO: "地理",
+  ICT: "資訊科技",
+  M1: "數學M1",
+  M2: "數學M2",
+  CLIT: "中國文學",
+  ELIT: "英國文學",
+  ALEVEL: "A-LEVEL",
+  SAT: "美國 SAT",
+  IB: "IB 課程",
+  IELTS: "IELTS 英文",
+  TOEFL: "TOEFL 英文",
+  PTH: "普通話",
+  OTHER: "其他科目",
+};
+
+module.exports = {
+  SUBJECTS,
+};
