5f575e66a8d58d3bf4ddeef96b10fcf13a8e4d8a
Louis Tsang <kawingt2@gmail.com>
2024-01-05 08:19:24 +0800

    copy update

--- bot/content/tutorContent.js
+++ bot/content/tutorContent.js
@@ -1,11 +1,14 @@
-const registrationConfirmation = `你已經完成登記，並獲得30點 Yo-points \n
-你可以輸入 /sc 
-    或\n
+const registrationConfirmation = `你已經完成登記，並獲得 3 個 Yo-points🔋\n
+你可以輸入 /sc\n 
+        或\n
 在 menu 按尋找學生去收生\n
-你每日最多可以聯絡 2 位學生\n
-假如你想聯絡更多學生🧑‍🎓👩‍🎓\n
+你每日最多可以聯絡 3 位學生
+假如你想聯絡更多學生🧑‍🎓👩‍🎓
 你便需要使用 Yo-points 🔋\n
-每次聯絡學生會扣除 10 點 Yo-points 🔋\n
+每次聯絡學生會扣除 1 個 Yo-point 🔋
+當你的 Yo-points 🔋 用完之後...\n
+你可以在 menu 購買 Yo-points 🔋
+或者訂閱月費計劃\
 `;
 
 const tutorContent = {
