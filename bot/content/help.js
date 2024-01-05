d0fa8fdd2e02e8697478fae8b4bda6602f9599cc
Louis Tsang <kawingt2@gmail.com>
2024-01-05 08:19:51 +0800

    wrote start text

---
+++ bot/content/help.js
@@ -0,0 +1,25 @@
+const startText = `
+哈囉～歡迎使用 Yo Tutor
+YoTutor 是1個 Telegram 補習配對機械人🤖
+可以為你免費配對學生或導師
+一般用戶每日最多可以聯絡 3 位學生
+假如你想聯絡更多學生🧑‍🎓👩‍🎓
+你便需要使用 Yo-points 🔋\n
+每次聯絡學生會扣除 1 個 Yo-point 🔋
+當你的 Yo-points 🔋 用完之後...\n
+你可以在 menu 購買 Yo-points 🔋
+或者訂閱月費計劃\n
+
+想立即試用🤔 可以從以下選項開始：
+
+登記成為導師，請按 /rt
+登記成為學生，請按 /rs
+查看導師資料，請按 /tp
+查看學生資料，請按 /stp
+想了解更多，請按 /help
+
+👆🏼以上選項同樣可以在左下角 menu ↙️找到\n`;
+
+module.exports = {
+  startText,
+};
