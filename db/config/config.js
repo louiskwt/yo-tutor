0f774e63ba74d768786e3ff4354aa716f1250908
Louis Tsang <kawingt2@gmail.com>
2024-01-10 08:16:47 +0800

    fixed config for migration

--- db/config/config.js
+++ db/config/config.js
@@ -1,9 +1,13 @@
+require("dotenv").config();
+
 const config = {
   development: {
     username: process.env.PG_USER || "root",
     password: process.env.PG_PW || "root",
     database: process.env.PG_DATABASE || "database_development",
     host: process.env.DB_HOST || "",
+    port: process.env.PG_PORT || "",
+    // use_env_variable: process.env.PG_HOST,
     dialect: "postgres",
   },
 };
