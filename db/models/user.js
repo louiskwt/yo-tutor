40d7b4e6d47cdcc522a69b0a4caa0037a83458c4
Louis Tsang <kawingt2@gmail.com>
2024-01-11 23:08:01 +0800

    fixed db orm association

--- db/models/user.js
+++ db/models/user.js
@@ -9,6 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      this.hasOne(models.Tutor, {foreignKey: "userId"});
     }
   }
   User.init(
