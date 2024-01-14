cba093b0baad9a840c882d8dc364271787549f22
Louis Tsang <kawingt2@gmail.com>
2024-01-14 23:50:49 +0800

    updated menu

--- bot/menu.js
+++ bot/menu.js
@@ -1,35 +1,11 @@
 const menu = {
-  rt: {
-    command: "rt",
-    description: "登記成為導師",
-  },
-  rs: {
-    command: "rs",
-    description: "登記成為學生",
-  },
-  tc: {
+  fc: {
     command: "tc",
     description: "尋找導師",
   },
-  sc: {
+  fc: {
     command: "sc",
     description: "尋找學生",
-  },
-  tp: {
-    command: "mytprofile",
-    description: "查看你的導師資料",
-  },
-  stp: {
-    command: "mystprofile",
-    description: "查看你的學生資料",
-  },
-  utp: {
-    command: "updatetprofile",
-    description: "更新你的導師資料",
-  },
-  ustp: {
-    command: "updatestprofile",
-    description: "更新你的學生資料",
   },
 };
 
